708d540298348f082de6712f905204ae18e8b728
Richard Lieknis <richard.lieknis@web.de>
2024-03-10 21:29:04 +0100

    host update

--- auth/settings.py
+++ auth/settings.py
@@ -41,8 +41,11 @@ ALLOWED_HOSTS = [
     'kanban.richardlieknis.com',
     'rychard93.eu.pythonanywhere.com',
     'authenticationapi-lfs6.onrender.com',
-
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
